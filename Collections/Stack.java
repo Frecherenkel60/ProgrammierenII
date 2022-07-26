497ac35dc80db18340a93c6f5a359b23819e6bd8
Frecherenkel60 <rieger.kevin01@gmail.com>
2022-07-26 18:05:11 +0200

    collection update

--- Collections/Stack.java
+++ Collections/Stack.java
@@ -1,5 +1,45 @@
 package Collections;
 
-public class Stack {
+public class Stack<T> {
+
+    private Node<T> top;
+    private int size = 0;
+
+    public void push(T data){
+        var newNode = new Node<T>(data);
+        size++;
+
+        if (top == null){
+            newNode.prev = top;
+        }
+        
+        top = newNode;
+        return;
+    }
+
+    public T pop(){
+        return null;
+    }
+
+    public T peek(){
+        return null;
+    }
+
+    public int size(){
+        return this.size;
+    }
     
+    // Internal data structure Node
+    private class Node<T> {
+
+        private T data;
+        private Node<T> prev;
+
+        Node(T data){
+            this.data = data;
+            this.prev = null;
+        }
+
+    }
+
 }
